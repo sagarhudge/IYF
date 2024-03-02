704c696271506af870c662a92c580b1a74f1d895
akashhudge-git <hudgeakash297@gmail.com>
2024-03-02 15:20:58 +0530

    Handle date picker

--- screens/src/schedule/component/NotificationDialog.tsx
+++ screens/src/schedule/component/NotificationDialog.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Button, Dialog, Portal, Text, TextInput, Title } from 'react-native-paper';
+import { Button, Dialog, Portal, Subheading, Text, TextInput, Title } from 'react-native-paper';
 import DateTimePicker, { Event } from '@react-native-community/datetimepicker';
 import DateTimePickerScreen from './TimePickerOption';
 import moment from 'moment';
 import TimePickerOption from './TimePickerOption';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { theme } from '../../../navigation/Index';
 
 
 type iProps = {
@@ -14,31 +16,27 @@ type iProps = {
 }
 
 const NotificationDialog: React.FC<iProps> = ({ visible, onSave, eventData }: iProps) => {
-    // const [visible, setVisible] = useState(false);
     const [startDate, setStartDate] = useState('08 03 2024');
-    const [endDate, setEndDate] = useState(new Date());
+    const [endDate, setEndDate] = useState('08 03 2024');
     const [showPicker, setShowPicker] = useState(false);
     const [pickerTitle, setPickerTitle] = useState('start');
 
     const [mode, setMode] = useState('date');
-    const [test, setText] = useState('Empty');
 
-    function showMode(currentMode: any) {
+    function showMode(currentMode: any, title: string) {
         setMode(currentMode)
+        setPickerTitle(title)
         setShowPicker(true);
     }
 
 
     function formatDates(date: any) {
-
         return moment(date).format('DD MM YYYY')
 
     }
 
     function formatTime(date: any) {
-
         return moment(date).format('DD MM YYYY HH:mm A')
-
     }
 
 
@@ -46,22 +44,21 @@ const NotificationDialog: React.FC<iProps> = ({ visible, onSave, eventData }: iP
 
         setShowPicker(false);
 
-
-        console.log('selectedDate', time, formatDates(time));
-
+        if (time == undefined) {
+            return;
+        }
         if (mode == 'date') {
-            setStartDate(formatDates(time));
-            showMode('time');
+            title === 'start' ? setStartDate(formatDates(time)) : setEndDate(formatDates(time))
+            showMode('time', title);
         } else if (mode === 'time') {
             setMode('date');
-            setStartDate(formatTime(time));
-        }
+            title === 'start' ? setStartDate(formatTime(time)) : setEndDate(formatTime(time))
 
+        }
 
     };
 
     function parseDate(dateStr: string) {
-        // Split the date string into day, month, and year components
         const [day, month, year] = dateStr.split(' ');
         return new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
     }
@@ -71,13 +68,22 @@ const NotificationDialog: React.FC<iProps> = ({ visible, onSave, eventData }: iP
             <Dialog.Title>Schedule Event</Dialog.Title>
             <Dialog.Content>
 
-                <TouchableOpacity onPress={() => { showMode('date') }}>
-                    <Title>{startDate}</Title>
+                <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }} onPress={() => { showMode('date', 'start') }}>
+                    <Subheading>{'Start From'}</Subheading>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Icon name={'calendar'} size={16} />
+
+                        <Subheading style={{ marginLeft: 8 }}>{startDate}</Subheading>
+                    </View>
                 </TouchableOpacity>
 
-                {/* <TouchableOpacity onPress={() => { showMode('date'); }}>
-                    <Title>{endDate()}</Title>
-                </TouchableOpacity> */}
+                <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }} onPress={() => { showMode('date', 'end'); }}>
+                    <Subheading>{'End At'}</Subheading>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Icon name={'calendar'} size={16} />
+                        <Subheading style={{ marginLeft: 8 }}>{endDate}</Subheading>
+                    </View>
+                </TouchableOpacity>
 
                 <TimePickerOption
                     showPicker={showPicker}
--- screens/src/schedule/component/TimePickerOption.tsx
+++ screens/src/schedule/component/TimePickerOption.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import { View, Button, Platform } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 
 type iPorps = {
-  showPicker: any, title: string, onChangeTime: ( time: Date | undefined, title: any) => void, time: Date, mode: any
+  showPicker: any, title: string, onChangeTime: (time: Date | undefined, title: any) => void, time: Date, mode: any
 }
 
 const TimePickerOption: React.FC<iPorps> = ({ showPicker, title, onChangeTime, time, mode }: iPorps) => {
 
-
-
   const handleDateChange = (event: any, date: Date | undefined) => {
-    console.log('time------>', date)
-    if (date) {
+    if (date && event.type !== 'dismissed') {
       onChangeTime(date, title)
+    }else{
+      onChangeTime(undefined, '')
+
     }
   };
 
@@ -22,7 +22,8 @@ const TimePickerOption: React.FC<iPorps> = ({ showPicker, title, onChangeTime, t
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       {showPicker && (
         <DateTimePicker
-          minimumDate={new Date()}
+          minimumDate={new Date(8, 3, 2024)}
+          maximumDate={new Date(14, 3, 2024)}
           testID="timePicker"
           value={time}
           mode={mode}
